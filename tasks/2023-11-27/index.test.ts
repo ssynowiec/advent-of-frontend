f2e7056dc12636fc0b6eeca53b0af0018f2bdd9d
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-27 10:50:32 +0100

    solution: 27/11/2023

--- tasks/2023-11-27/index.test.ts
+++ tasks/2023-11-27/index.test.ts
@@ -1,1 +1,21 @@
-// Tutaj skopiuj testy dla zadania. Uruchom je poleceniem `npm test`
+import { calculateTilesNeeded } from './index';
+
+describe('calculateTilesNeeded', () => {
+	test('returns correct tile count for single section', () => {
+		expect(calculateTilesNeeded([{ width: 5, height: 3 }])).toBe(15);
+	});
+
+	test('returns correct tile count for multiple sections', () => {
+		expect(
+			calculateTilesNeeded([
+				{ width: 5, height: 3 },
+				{ width: 2, height: 2 },
+				{ width: 3, height: 4 },
+			]),
+		).toBe(31);
+	});
+
+	test('returns zero when no sections are provided', () => {
+		expect(calculateTilesNeeded([])).toBe(0);
+	});
+});
--- tasks/2023-11-27/index.ts
+++ tasks/2023-11-27/index.ts
@@ -1,1 +1,11 @@
-// Tutaj skopiuj kod zadania
+export function calculateTilesNeeded(
+	sections: Array<{ width: number; height: number }>,
+): number {
+	let totalTiles = 0;
+
+	sections.forEach(section => {
+		totalTiles += section.height * section.width;
+	});
+
+	return totalTiles;
+}
